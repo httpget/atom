098de335be558853e3d95dff6bd2ba0cc9b7ac98
aiden <62084752+httpget@users.noreply.github.com>
2020-04-30 20:40:10 -0400

    Update api.lua

--- api.lua
+++ api.lua
@@ -1,4 +1,5 @@
-local api = {}
-api.t_dump = loadstring(game:HttpGet("https://raw.githubusercontent.com/httpget/atom/master/api/dumptables.lua", true));
-api.et_dump = loadstring(game:HttpGet("https://raw.githubusercontent.com/httpget/atom/master/api/dumpenvtables", true));
+local api = {
+  api.t_dump = loadstring(game:HttpGet("https://raw.githubusercontent.com/httpget/atom/master/api/dumptables.lua", true));
+  api.et_dump = loadstring(game:HttpGet("https://raw.githubusercontent.com/httpget/atom/master/api/dumpenvtables", true));
+}
 return api
